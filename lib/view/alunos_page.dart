7493d96de16de55fb57165e720d739f47a588477
Arthur <arthurbufon2052@gmail.com>
2023-09-05 21:47:40 -0300

    login page e alunos page

---
+++ lib/view/alunos_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_avaliacao_mobile3/model/aluno_model.dart';
+import 'package:flutter_avaliacao_mobile3/controller/aluno_controller.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class AlunosPage extends StatefulWidget {
+  const AlunosPage({super.key});
+
+  @override
+  State<AlunosPage> createState() => _AlunosPageState();
+}
+
+class _AlunosPageState extends State<AlunosPage> {
+  AlunoController alunoController = AlunoController();
+
+  late Future<List<AlunoEntity>> future;
+
+  Future<List<AlunoEntity>> getAlunos() async {
+    return alunoController.getAlunosList();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    future = getAlunos();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Alunos")),
+      ),
+      body: FutureBuilder<List<AlunoEntity>>(
+        future: future,
+        builder: (context, AsyncSnapshot<List<AlunoEntity>> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Slidable(
+                    endActionPane: ActionPane(
+                      motion: ScrollMotion(),
+                      children: [
+                        SlidableAction(
+                          onPressed: (context) {},
+                          backgroundColor: Colors.grey,
+                          foregroundColor: Colors.black,
+                          icon: Icons.edit,
+                          label: "Alterar",
+                        ),
+                        SlidableAction(
+                          onPressed: (context) {},
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white,
+                          icon: Icons.delete,
+                          label: "Excluir",
+                        ),
+                      ],
+                    ),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.brown.shade800,
+                        child: Text("A"),
+                      ),
+                      title: Text('${snapshot.data![index].nome}'),
+                      subtitle: Text('${snapshot.data![index].email}'),
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+}
